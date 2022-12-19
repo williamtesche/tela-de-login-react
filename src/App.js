141c2bc45b2266f3ad4c1d321392273abf073476
willian-matheus <85637800+williamtesche@users.noreply.github.com>
2022-12-19 07:38:15 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,62 +1,65 @@
 import { useState } from "react";
-import "./styles.css"
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./styles.css";
+import Cadastro from "./Cadastro";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
-
+  function Button() {
+    alert("Seu login foi Negado!");
+  }
 
   return (
     <div className="container">
       <div className="container-login">
         <div className="wrap-login">
-
           <form className="login-from">
-
             <span className="login-form-title">Bem Vindo!</span>
 
-
             <div className="wrap-input">
-
-              <input 
-              className={email !== "" ? 'has-val input' : 'input' }
-               type="Email"
+              <input
+                className={email !== "" ? "has-val input" : "input"}
+                type="Email"
                 value={email}
-                onChange={e => setEmail(e.target.value)}
-               />
+                onChange={(e) => setEmail(e.target.value)}
+              />
 
               <span className="focus-input" data-placeholder="Email"></span>
             </div>
 
             <div className="wrap-input">
-              <input 
-              c className={email !== "" ? 'has-val input' : 'input' } 
-              type="Password" 
-              value={password}
-              onChange={e => setPassword(e.target.value)}
+              <input
+                c
+                className={email !== "" ? "has-val input" : "input"}
+                type="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
               <span className="focus-input" data-placeholder="Password"></span>
             </div>
 
+            <div className="container-login-form-btn">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <button className="login-form-btn" onClick={Button}>
+                Login
+              </button>
+            </div>
+            <div className="text-center">
+              <span className="txt1">Não possui conta?</span>
+            </div>
 
-              <div className="container-login-form-btn">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <button className="login-form-btn">Login</button>
-              
-                
-              </div>
-
-
-              <div className="text-center">
-                <span className="txt1">Não possui conta?</span>
-
-                <a className="txt2" href="#">Criar conta.</a>
-              </div>
+            <Router>
+              <Route path="/" exact component={Cadastro}>
+                <a className="txt2" href="/">
+                  Criar conta.
+                </a>
+              </Route>
+            </Router>
           </form>
         </div>
       </div>
---
+++ src/Cadastro.js
@@ -0,0 +1,14 @@
+
+
+
+
+function Cadastro() {
+  return (
+    <div>bem vindo</div>
+
+  )
+
+  }
+
+
+export default Cadastro;
